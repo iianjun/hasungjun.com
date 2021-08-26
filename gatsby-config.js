834f03c64710e9d0f4ed174e33860043943b2715
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-08-26 01:08:25 -0400

    Initial commit from gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,21 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Hasung Jun",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
