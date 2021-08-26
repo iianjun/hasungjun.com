4c6c881db0d24b09e7892da07ddca1eced22de93
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-08-26 19:33:21 -0400

    Update Images

--- src/components/homeSections/contactSection/index.js
+++ src/components/homeSections/contactSection/index.js
@@ -7,7 +7,7 @@ export const ContactSection = () => {
         <G.SectionLink to="/contact">
             <G.Section url={Contact}>
                 <G.Typography paddingTop={50}>
-                    <G.Title color="#000000">Contact</G.Title>
+                    <G.Title color="#ffffff">Contact</G.Title>
                     <G.Info $weight={40}>Here is how you contact me</G.Info>
                     <LearnMore />
                 </G.Typography>
--- src/components/homeSections/projectSection/index.js
+++ src/components/homeSections/projectSection/index.js
@@ -1,20 +1,35 @@
 import React from "react";
 import * as G from "../../common/global-style";
-import SuniProjects from "../../../images/projects-large-suni.png";
+import Suni from "../../../images/projects-large-suni.png";
+import TinyDesk from "../../../images/projects-large-tinydesk.png";
 import { LearnMore } from "../../common";
 
 export const ProjectSection = () => {
     return (
-        <G.SectionLink to="/projects">
-            <G.Section url={SuniProjects}>
-                <G.Typography paddingTop={50}>
-                    <G.Title color="#ffffff">Suni</G.Title>
-                    <G.Info $weight={40}>
-                        Schedule Management iOS App for Students in SUNY Korea
-                    </G.Info>
-                    <LearnMore />
-                </G.Typography>
-            </G.Section>
-        </G.SectionLink>
+        <>
+            <G.SectionLink to="/projects">
+                <G.Section url={Suni}>
+                    <G.Typography paddingTop={50}>
+                        <G.Title color="#ffffff">Suni</G.Title>
+                        <G.Info $weight={40}>
+                            Schedule Management iOS App for Students in SUNY
+                            Korea
+                        </G.Info>
+                        <LearnMore />
+                    </G.Typography>
+                </G.Section>
+            </G.SectionLink>
+            <G.SectionLink to="/projects">
+                <G.Section url={TinyDesk}>
+                    <G.Typography paddingTop={50}>
+                        <G.Title color="#000000">Tiny Desk</G.Title>
+                        <G.Info $weight={40}>
+                            Change the way you organize your bookmarks
+                        </G.Info>
+                        <LearnMore />
+                    </G.Typography>
+                </G.Section>
+            </G.SectionLink>
+        </>
     );
 };
