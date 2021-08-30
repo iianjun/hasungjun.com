22d9a5b499f296072fadc52aa476bf8ed2abaca5
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-08-29 22:18:06 -0400

    Fix path for link

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -30,72 +30,72 @@ export const Layout = ({ pageTitle, children }) => {
                 <link
                     rel="shortcut icon"
                     type="image/png"
-                    href="src/images/favicon.ico"
+                    href="../images/favicon.ico"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="72x72"
-                    href="./images/apple-touch-icon-72x72.png"
+                    href="../images/apple-touch-icon-72x72.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="114x114"
-                    href="./images/apple-touch-icon-114x114.png"
+                    href="../images/apple-touch-icon-114x114.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="120x120"
-                    href="./images/apple-touch-icon-120x120.png"
+                    href="../images/apple-touch-icon-120x120.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="144x144"
-                    href="./images/apple-touch-icon-144x144.png"
+                    href="../images/apple-touch-icon-144x144.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="152x152"
-                    href="./images/apple-touch-icon-152x152.png"
+                    href="../images/apple-touch-icon-152x152.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
-                    href="./images/apple-touch-icon-180x180.png"
+                    href="../images/apple-touch-icon-180x180.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
-                    href="./images/favicon-16x16.png"
+                    href="../images/favicon-16x16.png"
                     sizes="16x16"
                 />
                 <link
                     rel="icon"
                     type="image/png"
-                    href="./images/favicon-32x32.png"
+                    href="../images/favicon-32x32.png"
                     sizes="32x32"
                 />
                 <link
                     rel="icon"
                     type="image/png"
-                    href="./images/favicon-96x96.png"
+                    href="../images/favicon-96x96.png"
                     sizes="96x96"
                 />
                 <link
                     rel="icon"
                     type="image/png"
-                    href="./images/favicon-128x128.png"
+                    href="../images/favicon-128x128.png"
                     sizes="128x128"
                 />
                 <link
                     rel="icon"
                     type="image/png"
-                    href="./images/favicon-192x192.png"
+                    href="../images/favicon-192x192.png"
                     sizes="192x192"
                 />
                 <link
                     rel="icon"
                     type="image/png"
-                    href="./images/favicon-196x196.png"
+                    href="..å/images/favicon-196x196.png"
                     sizes="196x196"
                 />
             </Helmet>
