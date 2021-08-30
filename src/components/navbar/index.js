3c8501dabb502b4ab97afa593a441222fe4e7e31
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-08-29 22:36:57 -0400

    Update nav font weight

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import * as S from './styled'
-import logo from '../../images/logo.svg'
+import React from "react";
+import * as S from "./styled";
+import logo from "../../images/logo.svg";
 
-import { Link } from 'gatsby'
+import { Link } from "gatsby";
 export const NavBar = () => {
     return (
         <S.Header>
@@ -45,17 +45,17 @@ export const NavBar = () => {
                         </S.NavItem>
                         <S.NavItem>
                             <Link to="/projects">
-                                <S.NavLink>Projects </S.NavLink>
+                                <S.NavLink>Projects</S.NavLink>
                             </Link>
                         </S.NavItem>
                         <S.NavItem>
                             <Link to="/contact">
-                                <S.NavLink>Contact </S.NavLink>
+                                <S.NavLink>Contact</S.NavLink>
                             </Link>
                         </S.NavItem>
                     </S.NavList>
                 </S.Nav>
             </S.Wrapper>
         </S.Header>
-    )
-}
+    );
+};
