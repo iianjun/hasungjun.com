e01d566df69e192a788f8afda7f392b246f51427
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-09-15 22:37:02 -0400

    Update Navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -8,52 +8,71 @@ export const NavBar = () => {
         <S.Header>
             <S.Wrapper className="container">
                 <S.Nav>
-                    <S.NavList className="mobile">
-                        <S.NavItem>
-                            <S.MobileMenu>
-                                <S.Line className="top" />
-                                <S.Line className="bottom" />
-                            </S.MobileMenu>
-                        </S.NavItem>
-                        <S.NavItem>
-                            {/* <S.NavLink className="logo">
-                                <img src={logo} onClick={onClickHome} />
-                            </S.NavLink> */}
-                        </S.NavItem>
-                    </S.NavList>
-                    <S.NavList className="full">
-                        <S.NavItem className="nav-item-hidden">
-                            <Link to="/">
-                                <S.NavLink>
-                                    <img
-                                        className="logo-full"
-                                        src={logo}
-                                        alt="logo"
-                                    />
-                                </S.NavLink>
-                            </Link>
-                        </S.NavItem>
-                        <S.NavItem>
-                            <Link to="/">
-                                <S.NavLink>Home</S.NavLink>
-                            </Link>
-                        </S.NavItem>
-                        <S.NavItem>
-                            <Link to="/about">
-                                <S.NavLink>About</S.NavLink>
-                            </Link>
-                        </S.NavItem>
-                        <S.NavItem>
-                            <Link to="/projects">
-                                <S.NavLink>Projects</S.NavLink>
-                            </Link>
-                        </S.NavItem>
-                        <S.NavItem>
-                            <Link to="/contact">
-                                <S.NavLink>Contact</S.NavLink>
-                            </Link>
-                        </S.NavItem>
-                    </S.NavList>
+                    <S.Mobile>
+                        <S.NavList>
+                            <S.NavLink href="#open-menu" role="button">
+                                <S.MenuIcon>
+                                    <S.MenuIconLabel>
+                                        <S.LineTopWrapper>
+                                            <S.LineTop />
+                                        </S.LineTopWrapper>
+                                        <S.LineBottomWrapper>
+                                            <S.LineBottom />
+                                        </S.LineBottomWrapper>
+                                    </S.MenuIconLabel>
+                                </S.MenuIcon>
+                            </S.NavLink>
+                            <S.MobileLogoWrapper>
+                                <S.NavItem className="mobile">
+                                    <Link to="/">
+                                        <S.NavLink>
+                                            <img
+                                                className="logo-full"
+                                                src={logo}
+                                                alt="logo"
+                                            />
+                                        </S.NavLink>
+                                    </Link>
+                                </S.NavItem>
+                            </S.MobileLogoWrapper>
+                        </S.NavList>
+                    </S.Mobile>
+                    <S.Full>
+                        <S.NavList>
+                            <S.NavItem className="nav-item-hidden">
+                                <Link to="/">
+                                    <S.NavLink>
+                                        <img
+                                            className="logo-full"
+                                            src={logo}
+                                            alt="logo"
+                                        />
+                                    </S.NavLink>
+                                </Link>
+                            </S.NavItem>
+                            <S.NavItem>
+                                <Link to="/">
+                                    <S.NavLink>Home</S.NavLink>
+                                </Link>
+                            </S.NavItem>
+                            <S.NavItem>
+                                <Link to="/about">
+                                    <S.NavLink>About</S.NavLink>
+                                </Link>
+                            </S.NavItem>
+                            <S.NavItem>
+                                <Link to="/projects">
+                                    <S.NavLink>Projects</S.NavLink>
+                                </Link>
+                            </S.NavItem>
+                            <S.NavItem>
+                                <Link to="/contact">
+                                    <S.NavLink>Contact</S.NavLink>
+                                </Link>
+                            </S.NavItem>
+                        </S.NavList>
+                    </S.Full>
+                    <S.Menu></S.Menu>
                 </S.Nav>
             </S.Wrapper>
         </S.Header>
--- src/components/navbar/styled.js
+++ src/components/navbar/styled.js
@@ -5,7 +5,10 @@ export const Header = styled.header`
     top: 0;
     z-index: 999;
     width: 100%;
-    height: 44px;
+    height: 48px;
+    @media (max-width: 767px) {
+        height: 44px;
+    }
     background-color: rgba(0, 0, 0, 0.8);
     backdrop-filter: saturate(180%) blur(20px);
 `;
@@ -18,23 +21,128 @@ export const NavList = styled.ul`
     align-items: center;
     justify-content: space-between;
     margin: 0 -10px;
-
-    .mobile {
-        display: none;
-    }
-    .full {
-    }
 `;
 export const NavItem = styled.li`
     cursor: pointer;
 `;
-export const MobileMenu = styled.div``;
-export const Line = styled.span`
-    .top {
-    }
-    .bottom {
+
+export const MobileLogoWrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export const Full = styled.div`
+    @media (max-width: 767px) {
+        display: none;
     }
 `;
+
+export const Mobile = styled.div`
+    display: none;
+    @media (max-width: 767px) {
+        display: block;
+    }
+`;
+
+export const MenuIcon = styled.li`
+    left: 0;
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    width: 48px;
+    border-bottom: none;
+`;
+
+export const MenuIconLabel = styled.label`
+    display: block;
+    position: absolute;
+    opacity: 0.8;
+    z-index: 3;
+    top: 0;
+    width: 48px;
+    height: 48px;
+    cursor: pointer;
+    -webkit-transition: opacity 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+    transition: opacity 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`;
+
+export const LineTopWrapper = styled.span`
+    position: absolute;
+    z-index: 3;
+    top: 9px;
+    left: 9px;
+    width: 30px;
+    height: 30px;
+    -webkit-transition: opacity 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+    transition: opacity 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+    -webkit-transition: -webkit-transform 0.1806s
+        cubic-bezier(0.04, 0.04, 0.12, 0.96);
+    transition: transform 0.1806s cubic-bezier(0.04, 0.04, 0.12, 0.96),
+        -webkit-transform 0.1806s cubic-bezier(0.04, 0.04, 0.12, 0.96);
+    -webkit-transform: none;
+    transform: none;
+    z-index: 4;
+`;
+export const LineBottomWrapper = styled.span`
+    position: absolute;
+    z-index: 3;
+    top: 9px;
+    left: 9px;
+    width: 30px;
+    height: 30px;
+    -webkit-transition: opacity 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+    transition: opacity 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+    -webkit-transition: -webkit-transform 0.1806s
+        cubic-bezier(0.04, 0.04, 0.12, 0.96);
+    transition: -webkit-transform 0.1806s cubic-bezier(0.04, 0.04, 0.12, 0.96);
+    transition: transform 0.1806s cubic-bezier(0.04, 0.04, 0.12, 0.96);
+    transition: transform 0.1806s cubic-bezier(0.04, 0.04, 0.12, 0.96),
+        -webkit-transform 0.1806s cubic-bezier(0.04, 0.04, 0.12, 0.96);
+    -webkit-transform: none;
+    transform: none;
+`;
+export const LineTop = styled.span`
+    display: block;
+    width: 17px;
+    height: 1px;
+    background: #fff;
+    border-radius: 0.5px;
+    position: absolute;
+    left: 7px;
+    z-index: 1;
+    top: 14px;
+    -webkit-transform: translateY(-3px);
+    transform: translateY(-3px);
+    -webkit-transition: -webkit-transform 0.1596s
+        cubic-bezier(0.52, 0.16, 0.52, 0.84) 0.1008s;
+    transition: -webkit-transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84)
+        0.1008s;
+    transition: transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84) 0.1008s;
+    transition: transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84) 0.1008s,
+        -webkit-transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84) 0.1008s;
+`;
+export const LineBottom = styled.span`
+    display: block;
+    width: 17px;
+    height: 1px;
+    background: #fff;
+    border-radius: 0.5px;
+    position: absolute;
+    left: 7px;
+    z-index: 1;
+    bottom: 14px;
+    -webkit-transform: translateY(3px);
+    transform: translateY(3px);
+    -webkit-transition: -webkit-transform 0.1596s
+        cubic-bezier(0.52, 0.16, 0.52, 0.84) 0.1008s;
+    transition: -webkit-transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84)
+        0.1008s;
+    transition: transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84) 0.1008s;
+    transition: transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84) 0.1008s,
+        -webkit-transform 0.1596s cubic-bezier(0.52, 0.16, 0.52, 0.84) 0.1008s;
+`;
+
 export const NavLink = styled.a`
     font-size: 1.4rem;
     color: #f5f5f7;
@@ -49,5 +157,30 @@ export const NavLink = styled.a`
         height: 44px;
         display: flex;
         align-items: center;
+    }
+`;
+
+export const Menu = styled.ul`
+    @media (max-width: 767px) {
+        #open-menu:checked {
+            display: block;
+            position: absolute;
+            z-index: 1;
+            top: 3.52941em;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            max-width: 664px;
+            margin: 0 auto;
+            padding: 45px 40px 8px;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+            overflow-y: auto;
+            -webkit-overflow-scrolling: touch;
+            visibility: hidden;
+            -webkit-transition: visibility 0s linear 1s;
+            transition: visibility 0s linear 1s;
+        }
     }
 `;
