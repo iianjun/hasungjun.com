ea87a9f392c338620a49a96f103c1ffff9e90d15
iianjun <iianjun@naver.com>
2024-06-15 19:07:57 -0700

    Update navbar

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React from "react";
+import classNames from "classnames";
+import styles from "@/styles/nav.module.scss";
+const NavBar: React.FC = () => {
+  return (
+    <header>
+      <nav>
+        <div className={styles.container}>
+          <Link href="/" className={styles.logo}>
+            <h1>Hasung Jun</h1>
+          </Link>
+          <ul className={styles.list}>
+            <li className={styles["list-item"]}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={styles["list-item"]}>
+              <Link href="/about">About</Link>
+            </li>
+            <li className={styles["list-item"]}>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li className={styles["list-item"]}>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
