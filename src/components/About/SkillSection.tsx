5b3a671936cea428dbf4822deeef0aa4602796d9
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2024-07-31 22:01:05 -0700

    Fix minor

--- src/components/About/SkillSection.tsx
+++ src/components/About/SkillSection.tsx
@@ -19,7 +19,7 @@ const SkillSection = () => {
         </h2>
         <div className="absolute h-full w-full overflow-hidden">
           <div
-            className="absolute top-1/2 flex items-center gap-24 ps-[calc(50%-10rem)] opacity-0 will-change-[transform,opacity]"
+            className="absolute top-1/2 flex items-center gap-24 ps-[calc(50%+15rem)] opacity-0 will-change-[transform,opacity] md:ps-[calc(50%+10rem)]"
             style={{ transform: `matrix(1, 0, 0, 1, 0, ${OFFSET})` }}
             ref={skills}
           >
@@ -29,10 +29,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 Typescript
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/typescript-logo.svg"
@@ -48,10 +45,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 Javascript
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/javascript-logo.svg"
@@ -67,10 +61,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 GraphQL
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/graphql-logo.svg"
@@ -86,10 +77,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 Next.js
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/nextjs-logo.svg"
@@ -105,10 +93,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 Node.js
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/nodejs-logo.svg"
@@ -124,10 +109,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 React
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/react-logo.svg"
@@ -143,10 +125,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 Cascading Style Sheets
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/css-logo.svg"
@@ -162,10 +141,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 Tailwind CSS
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/tailwindcss-logo.svg"
@@ -181,10 +157,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 Nest.js
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/nestjs-logo.svg"
@@ -201,10 +174,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 MongoDB
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/mongodb-logo.svg"
@@ -220,10 +190,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 SQL
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/sql-logo.svg"
@@ -239,10 +206,7 @@ const SkillSection = () => {
                 className="absolute bottom-[calc(100%+2rem)] left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-2xl bg-[#383838] px-6 py-4 text-2xl font-semibold text-white opacity-0 shadow-sm"
               >
                 REST API
-                <div
-                  className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit"
-                  data-popper-arrow
-                ></div>
+                <div className="absolute left-1/2 h-6 w-6 -translate-x-1/2 rotate-45 bg-inherit" />
               </div>
               <Image
                 src="/skills/rest-api-logo.svg"
